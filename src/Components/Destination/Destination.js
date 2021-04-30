70b28d1e8c14989fe7627846108105da07aa4906
unknown <you@example.com>
2021-05-01 03:53:20 +0600

    add map

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import Header from '../Header/Header';
-
+import './Destination.css'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 const Destination = () => {
     return (
-        <div>
+        <Container>
             <Header></Header>
-            <h1>this is destination</h1>
-        </div>
+            <br />
+            <Row>
+                <Col md={4}>
+                    <h1>hi</h1>
+                </Col>
+                <Col md={8}>
+                <iframe title='this is map' src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7306.729673488774!2d90.38065920000001!3d23.6986621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1619818391712!5m2!1sen!2sbd" width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
-export default Destination;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyB2LyOPbgcJZF2mmVuvbifev81PAg3JEIo'
+})(Destination);
--- src/Components/Rides/Rides.js
+++ src/Components/Rides/Rides.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
-import'./Riders.css'
+import { useHistory } from 'react-router';
+import { UserContext } from '../../App';
+import './Riders.css'
 const Rides = (props) => {
-    const { name, img } = props.ride
+    const { name, img } = props.ride;
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory()
+    const selectedRide = () => {
+        history.push('/destination')
+        const rideInfo = {...loggedInUser}
+        rideInfo.rideName=name;
+        rideInfo.rideImg=img;
+        setLoggedInUser(rideInfo)
+    }
     return (
 
         <Col md={3}>
             <Card className='card-style'>
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
-                    <Button variant="primary">{name}</Button>
+                    <Button variant="primary" onClick={selectedRide}>{name}</Button>
                 </Card.Body>
             </Card>
         </Col>
